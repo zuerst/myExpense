205a519dc48ae2124ec1c011a0c806eaed6eba41
Kevin <kevin.kang1110@gmail.com>
2015-05-11 21:22:48 -0700

    registration page moved

--- myExpense/myExpense/static/js/myExpense.js
+++ myExpense/myExpense/static/js/myExpense.js
@@ -69,3 +69,18 @@ function deleteEntry(transId) {
     })
 }
 
+// Add bootstrap model popup listener. For login in PopUp in mainPage
+$(document).ready(function() {
+	$('#loginPopup').on('shown.bs.modal', function () {
+		$('#myInput').focus()
+	})
+
+	$('#registerPopup').on('shown.bs.modal', function () {
+		$('#myInput').focus()
+	})
+
+	$('#loginFromRegPopup').click(function() {
+		$('#loginPopup').modal('show')
+		$('#registerPopup').modal('hide')
+	})
+})
